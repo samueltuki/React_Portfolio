3a71ebf0062e5b55f02722a19e3ddbd0bd7b3da7
samuel_tuki <smauel_tuki@yahoo.com>
2023-03-12 01:56:42 +0000

    project section responsiveness

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,9 +2,36 @@ import React from "react";
 
 function Projects(props) {
   return (
-    <div>
-      
+<div>
+<div class="container-fluid" id="projects">
+    <h2 class="text-center py-4 display-4 text-danger pt-5">Projects</h2>
+    <div class="row ">
+
+        
+      <div id="project1" class="col-12 col-sm-12 col-md-8 col-lg-6 d-flex align-items-stretch">
+        <div class="card mb-3">
+          <img 
+            src={props.image}
+            class="card-img-top"
+            alt="..."/>
+                        
+          <div class="card-body">
+            <h5 class="card-title">{props.title}</h5>
+            <p class="card-text">
+             <span class="small">{props.description}</span>
+            </p>
+            <a href={props.deployed} target="_blank">Deploy site</a>
+              
+          </div>
+        </div>
+      </div>
+   
+  
+  
+ 
     </div>
+  </div>
+</div>
   );
 }
 
