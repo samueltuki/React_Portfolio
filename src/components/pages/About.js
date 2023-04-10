83677fce4f1e8f9bfbe5ad3d4d55ac41879b2866
samuel_tuki <smauel_tuki@yahoo.com>
2023-04-10 22:42:23 +0100

    navbar mobile collapse

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import { Navlink } from 'react-router-dom';
-import displayImg from "../../components/images/stt.jpg"
+import displayImg from "../../components/images/stt.jpg";
 import { Link } from "react-router-dom";
 
 function About() {
@@ -12,7 +12,7 @@ function About() {
             <img
               src={displayImg}
               className="rounded-circle"
-              width="240rem"
+              width="280rem"
               alt="displayPicture"
             />
           </div>
@@ -22,23 +22,20 @@ function About() {
               Hi, I am Sam a Front-End Web Developer.
             </p>
             <p className=" lead mt-3 h5 about-me-content">
-            I have a keen eye for designing and creating beautiful and responsive websites. I do enjoy watching soccer and rap music.
+              I have a keen eye for designing and creating beautiful and
+              responsive websites. I do enjoy watching soccer and rap music.
             </p>
-            
-          
+
             <div>
-            <Link to="/contact">
-                <button className="btn btn-danger mt-5 mb-5">Get In-touch</button>
+              <Link to="/contact">
+                <button className="btn btn-danger mt-5 mb-5">
+                  Get In-touch
+                </button>
               </Link>
             </div>
           </div>
-
-
-         
         </div>
       </div>
-
-     
     </div>
   );
 }
