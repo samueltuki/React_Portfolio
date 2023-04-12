e80bfbe19f584022bc2dedcfd6d959a20f8d1a49
samuel_tuki <smauel_tuki@yahoo.com>
2023-04-12 22:43:12 +0100

    useRef to handle mobile nav

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useRef } from "react";
 import { NavLink } from "react-router-dom";
 
 function NavTabs() {
+  const mobileToggleRef = useRef(null);
+
+  const handleNavLinkClick = () => {
+    // Simulate a click on the mobile toggle button to hide the menu
+    mobileToggleRef.current.click();
+  };
+
   return (
     <div className="d-flex justify-content-between navvv navStick nav-bar ">
       <nav
@@ -14,6 +21,7 @@ function NavTabs() {
           data-toggle="collapse"
           data-target="#collapsibleNavbar"
           aria-aria-expanded="false"
+          ref={mobileToggleRef} // Add a ref to the mobile toggle button
         >
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -26,6 +34,7 @@ function NavTabs() {
                 className={({ isActive }) =>
                   isActive ? "nav-link active" : "nav-link"
                 }
+                onClick={handleNavLinkClick} // Add onClick event listener to hide the menu
               >
                 About
               </NavLink>
@@ -36,6 +45,7 @@ function NavTabs() {
                 className={({ isActive }) =>
                   isActive ? "nav-link active" : "nav-link"
                 }
+                onClick={handleNavLinkClick} // Add onClick event listener to hide the menu
               >
                 Project
               </NavLink>
@@ -47,6 +57,7 @@ function NavTabs() {
                 className={({ isActive }) =>
                   isActive ? "nav-link active" : "nav-link"
                 }
+                onClick={handleNavLinkClick} // Add onClick event listener to hide the menu
               >
                 Contact
               </NavLink>
@@ -57,6 +68,7 @@ function NavTabs() {
                 className={({ isActive }) =>
                   isActive ? "nav-link active" : "nav-link"
                 }
+                onClick={handleNavLinkClick} // Add onClick event listener to hide the menu
               >
                 Skills
               </NavLink>
@@ -69,3 +81,75 @@ function NavTabs() {
 }
 
 export default NavTabs;
+
+// import React from "react";
+// import { NavLink } from "react-router-dom";
+
+// function NavTabs() {
+//   return (
+//     <div className="d-flex justify-content-between navvv navStick nav-bar ">
+//       <nav
+//         className="navbar navbar-expand-md nav-fill fixed-top"
+//         style={{ height: "35px" }}
+//       >
+//         <button
+//           className="navbar-toggler collapsed"
+//           type="button"
+//           data-toggle="collapse"
+//           data-target="#collapsibleNavbar"
+//           aria-aria-expanded="false"
+//         >
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
+//         {/* navbar links */}
+//         <div className="collapse navbar-collapse navBg" id="collapsibleNavbar">
+//           <ul className="navbar-nav">
+//             <li className="nav-item mt-4">
+//               <NavLink
+//                 to="about"
+//                 className={({ isActive }) =>
+//                   isActive ? "nav-link active" : "nav-link"
+//                 }
+//               >
+//                 About
+//               </NavLink>
+//             </li>
+//             <li className="nav-item mt-4">
+//               <NavLink
+//                 to="projectGallery"
+//                 className={({ isActive }) =>
+//                   isActive ? "nav-link active" : "nav-link"
+//                 }
+//               >
+//                 Project
+//               </NavLink>
+//             </li>
+//             <li className="nav-item mt-4">
+//               <NavLink
+//                 to="contact"
+//                 end
+//                 className={({ isActive }) =>
+//                   isActive ? "nav-link active" : "nav-link"
+//                 }
+//               >
+//                 Contact
+//               </NavLink>
+//             </li>
+//             <li className="nav-item  mt-4">
+//               <NavLink
+//                 to="skills"
+//                 className={({ isActive }) =>
+//                   isActive ? "nav-link active" : "nav-link"
+//                 }
+//               >
+//                 Skills
+//               </NavLink>
+//             </li>
+//           </ul>
+//         </div>
+//       </nav>
+//     </div>
+//   );
+// }
+
+// export default NavTabs;
